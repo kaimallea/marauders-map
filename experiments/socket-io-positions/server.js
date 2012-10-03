0af122f5c78d6f63b7a7efda077c36a8a3771e23
Kai Mallea <kmallea@gmail.com>
2012-10-03 02:51:15 -0400

    Parse packets on persistent socket

--- experiments/socket-io-positions/server.js
+++ experiments/socket-io-positions/server.js
@@ -13,16 +13,33 @@ var http = require('http'),
     webapp = express(), // func call
     io = require('socket.io');
 
-httpServer = http.createServer(webapp);
-webSocketServer = io.listen(httpServer);
-tcpServer = net.createServer(function (socket) {
+var httpServer = http.createServer(webapp);
+var webSocketServer = io.listen(httpServer);
+var tcpServer = net.createServer(function (socket) {
     socket.setKeepAlive(true, 5);
     socket.setEncoding('utf-8');
     socket.on('data', dataProcessor);
 });
 
+var buffer = ''; // buffer for incoming packet data
 var dataProcessor = function (data) {
-    console.log(data);
+    buffer += data.toString(); // convert incoming data to a string and store in buffer
+
+    // since the connection stays open we need to check for \r\n
+    // to know when a packet is complete
+    var packetEnd = buffer.indexOf('\r\n');
+    if (packetEnd !== -1) {  // there's a complete packet in the buffer
+        var json = buffer.slice(0, packetEnd); // extract complete part (which should be parsable json)
+        buffer = buffer.slice(packetEnd+2); // chop off completed packet from buffer
+        try {
+            json = JSON.parse(json);
+        } catch (e) {
+            console.log('Bad json.\nAttempted to parse: ' + json + '\nCaptured Error: ' + e);
+            return;
+        }
+
+        console.log(json);
+    }
 };
 
 // HTTP requests for static files should match file
