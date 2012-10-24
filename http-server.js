4e3b55d30990b7f95abbbcc75e33a1752a01cd04
Kai Mallea <kmallea@gmail.com>
2012-10-24 12:16:45 -0400

    Forward inc UDP data to web sockets

--- http-server.js
+++ http-server.js
@@ -1,12 +1,39 @@
 #!/usr/bin/env node
 
-var HTTP_PORT = 1337;
+var HTTP_PORT = 1337,
+    UDP_PORT = 1338;
 
 var http = require('http'),
-    net = require('net'),
+    dgram = require('dgram'),
     express = require('express'),
-    webapp = express(),
-    server = http.createServer(webapp);
+    io = require('socket.io'),
+    util = require('util');
+
+var webapp = express();
+var httpServer = http.createServer(webapp);
+var webSocketServer = io.listen(httpServer);
+var udpServer = dgram.createSocket('udp4');
+
+
+// Keep track of game state, so clients don't have to
+var GAME_STATE = {
+    t: {
+        teamName: '',
+        wins: 0
+    },
+
+    ct: {
+        teamName: '',
+        wins: 0
+    },
+
+    mapName: '',
+    currentRound: 0,
+    maxRounds: 0,
+    warmup: true,
+    halftime: false
+};
+
 
 // Static files (js, css, images, etc.) will be
 // served out of "static" folder
@@ -18,6 +45,30 @@ webapp.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-console.log('Listening on %d', HTTP_PORT);
 
-server.listen(HTTP_PORT);
+// Handle incoming UDP packets
+udpServer.on('message', function (msg, rinfo) {
+    var data = msg.split(',');
+
+    switch(data[0]) {
+        // Positions
+        case 'p':
+            webSocketServer.sockets.emit('positions',
+                // id,team,bomb,x,y,z,yaw
+                util.format('%d,%d,%d,%f,%f,%f,%f', data[1], data[2], data[3], data[4], data[5], data[6], data[7])
+            );
+            break;
+
+        // Unknown
+        default:
+    }
+});
+
+udpServer.on('listening', function () {
+    console.log('UDP server listening on %d', UDP_PORT);
+});
+
+httpServer.listen(HTTP_PORT);
+console.log('HTTP server listening on %d', HTTP_PORT);
+
+udpServer.bind(UDP_PORT);
